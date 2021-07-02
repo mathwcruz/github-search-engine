fa5215159a3381bfb1a29ff30c1775aa4d03f02a
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-07-02 20:44:44 -0300

    feat: create search-box content and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,50 @@
+import { useCallback } from 'react';
 import Image from 'next/image';
-import {} from 'antd';
+import { Form, Input, Button } from 'antd';
 
 import styles from 'styles/pages/Home.module.scss';
 
 export default function Home() {
+  const [form] = Form.useForm();
+
+  const handleSubmitSearchOrgs = useCallback((field) => {
+    console.log({ field });
+    form.validateFields();
+  }, []);
+
   return (
     <div className={styles.homeContainer}>
-      <Image
-        src='/images/background-banner.png'
-        className={styles.backgroundBanner}
-        alt='Logo do Github'
-        width={500}
-        height={500}
-      />
+      <div className={styles.backgroundBanner}>
+        <Image
+          src='/images/logo.svg'
+          alt='github-search-engine'
+          className={styles.logoImg}
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className={styles.searchBox}>
+        <h2>Explore repositórios no Github.</h2>
+        <Form form={form} onFinish={handleSubmitSearchOrgs}>
+          <Form.Item
+            className={styles.searchOrgsItem}
+            name='orgs'
+            rules={[
+              {
+                required: true,
+                message: 'Insira o nome de uma organização do Github',
+              },
+            ]}
+          >
+            <Input
+              className={styles.searchOrgsInput}
+              bordered={false}
+              placeholder='Digite aqui'
+            />
+          </Form.Item>
+          <button type='submit'>Pesquisar</button>
+        </Form>
+      </div>
     </div>
   );
 }
