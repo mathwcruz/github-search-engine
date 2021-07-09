71ad5bb7b16661ee8ff967b93f1021ec2654e220
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-07-08 21:11:18 -0300

    feat: add initial user-profile content

--- src/pages/users/[slug].tsx
+++ src/pages/users/[slug].tsx
@@ -1,4 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 type User = {
   userId: string;
@@ -25,13 +28,61 @@ interface UserProps {
   };
 }
 
+import styles from 'styles/pages/User.module.scss';
+
 import { api } from 'services/api';
 
 export default function User({ user }: UserProps) {
   console.log({ user });
+
   return (
-    <div>
-      <h1>usuário: {user?.profile?.name}</h1>
+    <div className={styles.userContainer}>
+      <div className={styles.backgroundBanner}>
+        <Image
+          src='/images/logo.svg'
+          alt='github-search-engine'
+          className={styles.logoImg}
+          width={200}
+          height={200}
+        />
+        <Link href='/'>
+          <a>
+            <AiOutlineArrowLeft size={20} color='#a8a8b3' />
+            Voltar
+          </a>
+        </Link>
+      </div>
+
+      <div className={styles.userProfile}>
+        <div className={styles.userInformations}>
+          {/* perguntar a comunidade como resolver o erro q ta dando ao usar o <Image /> do Next com imagens externas */}
+          <img src={user?.profile?.avatar} alt={user?.profile?.name} />
+          <div>
+            <h4>{user?.profile?.name}</h4>
+            <p>{user?.profile?.bio}</p>
+          </div>
+        </div>
+        <section className={styles.userGithubData}>
+          {/* componentizar essas div's */}
+          <div>
+            <strong>{user?.profile?.followers}</strong>
+            <small>Seguidores</small>
+          </div>
+          <div>
+            <strong>{user?.profile?.following}</strong>
+            <small>Seguindo</small>
+          </div>
+          <div>
+            <strong>{user?.profile?.publicRepos}</strong>
+            <small>Repos públicos</small>
+          </div>
+        </section>
+      </div>
+      {/* <div className={styles.userRepositories}>
+        <ul>
+          <li></li>
+        </ul>
+      </div> */}
     </div>
   );
 }
@@ -64,7 +115,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     publicRepos: userInfo?.public_repos,
   };
 
-  const repositories = userRepositories?.map((repo) => {
+  const repositories = userRepositories?.slice(0, 5)?.map((repo) => {
     return {
       id: repo?.id,
       name: repo?.name,
