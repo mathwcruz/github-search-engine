27e9b5909ebdbf03ee57ef0174973a15d3c201fc
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-07-09 20:19:14 -0300

    feat: create user-repositories content and style

--- src/components/UserRepositories.tsx
+++ src/components/UserRepositories.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+
 import { Repository } from 'pages/users/[slug]';
 
 import styles from 'styles/components/UserRepositories.module.scss';
@@ -12,9 +15,38 @@ export function UserRepositories({ repositories }: UserRepositoriesProps) {
   return (
     <div className={styles.userRepositories}>
       <ul>
-        <li>teste 1</li>
-        <li>teste 2</li>
-        <li>teste 3</li>
+        <li>
+          <div>
+            <h3>{repositories[0]?.name}</h3>
+            <a
+              href={repositories[0]?.url}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <AiOutlineArrowRight color='#a8a8b3' size={20} />
+            </a>
+          </div>
+          <section>
+            <p>{repositories[0]?.description}</p>
+            <strong>Última edição em {repositories[0]?.updatedAt}</strong>
+          </section>
+        </li>
+        <li>
+          <div>
+            <h3>{repositories[1]?.name}</h3>
+            <a
+              href={repositories[1]?.url}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <AiOutlineArrowRight color='#a8a8b3' size={20} />
+            </a>
+          </div>
+          <section>
+            <p>{repositories[1]?.description}</p>
+            <strong>Última edição em {repositories[1]?.updatedAt}</strong>
+          </section>
+        </li>
       </ul>
       {/* caso o user nao tenha nenhum repo público, mostrar uma <Empty /> message */}
     </div>
